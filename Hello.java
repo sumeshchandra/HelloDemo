297134c87af69ca039926fa75539532cad04220c
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-07-10 09:17:52 +0530

    Added Employee is Present or Not

--- Hello.java
+++ Hello.java
@@ -1,8 +1,15 @@
 class Hello
 {
-	public static void main(String[] args){
-
+	public static void main(String[] args)
+	{
 	System.out.print("Hello Worls");
-}
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println(" Employee is Present");
+		else
+			 System.out.println(" Employee is Absent");
+
+	}
 
 }
