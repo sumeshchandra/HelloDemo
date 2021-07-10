ac1fbc8154e936fac2f734cbb468c9c1a6a8027e
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-07-10 09:41:00 +0530

    Added: Wage Hour

--- Hello.java
+++ Hello.java
@@ -3,6 +3,9 @@ class Hello
 	public static void main(String[] args)
 	{
 	System.out.print("Hello Worls");
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int dailyWage = 0;
 		int IS_FULL_TIME = 1;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
